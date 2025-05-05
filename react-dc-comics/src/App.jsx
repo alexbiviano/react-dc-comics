a4b2c92d43399a09b19d79eacd2ea7b903d48a6c
alexbiviano <bivianoalex63@gmail.com>
2025-05-05 16:47:44 +0200

    [fix] fix images

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import Header from './components/Header'
+// importo i miei components
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
-const App = () => {
+function App() {
+
   return (
     <>
       <Header />
+      <Main />
+      <Footer />
     </>
+
   )
 }
 
--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import NavBar from './NavBar'
 const Header = () => {
     return (
         <header>
             <div className="container">
-                <figure>
-                    <img src="/dc-logo.png" alt="Logo DC" />
-                </figure>
-                <NavBar />
+                <div className="header-style">
+                    <figure>
+                        <img src="../src/assets/img/dc-logo.png" alt="" />
+                    </figure>
+
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </div>
+
             </div>
+
         </header>
     )
 }
 
-export default Header
+export default Header;
