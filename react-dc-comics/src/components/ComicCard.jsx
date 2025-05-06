b601fced5e8fe14a800e8ac860c3eef7fdcccedd
alexbiviano <bivianoalex63@gmail.com>
2025-05-06 15:23:05 +0200

    [upd] create card

---
+++ react-dc-comics/src/components/ComicCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ComicCard() {
+    return (
+        <div className='container'>
+            <div className='card'>
+                <div className='card-body'>
+                    <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" alt="" />
+                    <div className='title'>Action Comics</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ComicCard
