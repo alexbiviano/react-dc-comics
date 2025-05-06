a70566dbef1db26563e53ae9bfd08e60d33aa596
alexbiviano <bivianoalex63@gmail.com>
2025-05-06 17:27:21 +0200

    [upd] immagini mostrate in ordine

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import ComicCard from './ComicCard'
+import ListComics from "./ListComics"
 
 const Main = () => {
     return (
-        <>
-            <div className='jumbotron'></div>
-            <div className='main-black'>
-                <div className='container'>
-                    <ComicCard />
+        <main>
+            <div className="main-top">
+                <div className="container">
+                    <ListComics />
                 </div>
             </div>
-        </>
+            <div className="main-bottom">
+                <div className="container">
+                    <ul>
+                        <li>
+                            <img src="src/assets/img/buy-comics-digital-comics.png" alt="" />
+                            DIGITAL COMICS
+                        </li>
+                        <li>
+                            <img src="src/assets/buy-comics-merchandise.png" alt="" />
+                            DC MERCHANDISE
+                        </li>
+                        <li>
+                            <img src="src/assets/img/buy-comics-subscriptions.png" alt="" />
+                            SUBSCRIPTION
+                        </li>
+                        <li>
+                            <img src="src/assets/img/buy-comics-shop-locator.png" alt="" />
+                            COMIC SHOP LOCATOR
+                        </li>
+                        <li>
+                            <img src="src/assets/img/buy-dc-power-visa.svg" alt="" />
+                            DC POWER VISA
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </main>
     )
 }
 
